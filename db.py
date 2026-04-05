b403386f5c32a89608910e527a83ce58b6874ac9
Ashleigh Hewitt <tom_94_oasis@hotmail.com>
2026-04-05 18:06:44 +0100

    InputBuffer

--- db.py
+++ db.py
@@ -2,13 +2,13 @@ import sys
 from dataclasses import dataclass
 
 @dataclass 
-class input_buffer: 
+class InputBuffer: 
     buffer: str = None
     buffer_length: int 
     input_length: int
 
-def new_input_buffer() -> input_buffer:
-    input_buffer = input_buffer()
+def new_input_buffer() -> InputBuffer:
+    input_buffer = InputBuffer()
     input_buffer.buffer_length = 0
     input_buffer.input_length = 0
     return input_buffer
